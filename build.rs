caf9d692e098500e1cdc34261a6ddc0350965402
Teruki Tada <me@peruki.dev>
2025-03-03 14:02:44 +0900

    build

--- build.rs
+++ build.rs
@@ -2,7 +2,6 @@ extern crate flatc_rust;
 use std::path::Path;
 
 fn main() {
-    println!("cargo:rerun-if-changed=schema/particlemap.fbs");
     flatc_rust::run(flatc_rust::Args {
         inputs: &[Path::new("schema/particlemap.fbs")],
         out_dir: Path::new("src/map"),
