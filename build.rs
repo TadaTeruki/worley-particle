f623851ce47a07b142e6fbc466b05dcf088249a4
Teruki TADA <me@peruki.dev>
2025-04-02 12:21:07 +0900

    protobuf

--- build.rs
+++ build.rs
@@ -1,11 +1,5 @@
-extern crate flatc_rust;
-use std::path::Path;
+extern crate prost_build;
 
 fn main() {
-    flatc_rust::run(flatc_rust::Args {
-        inputs: &[Path::new("schema/particlemap.fbs")],
-        out_dir: Path::new("src/map/rw"),
-        ..Default::default()
-    })
-    .expect("flatc failed to run");
+    prost_build::compile_protos(&["schema/particlemap.proto"], &["schema/"]).unwrap();
 }
--- src/map/rw/mod.rs
+++ src/map/rw/mod.rs
@@ -1,19 +1,19 @@
 use std::{collections::HashMap, error::Error, io::Write};
 
-use flatbuffers::FlatBufferBuilder;
-use particlemap_generated::particlemap::items::{
-    finish_fbs_particle_map_buffer, root_as_fbs_particle_map, FbsParticle, FbsParticleArgs,
-    FbsParticleMap, FbsParticleMapArgs, FbsParticleParameters, FbsParticleParametersArgs,
-};
+use particlemap::items::{MsgParticle, MsgParticleMap, MsgParticleParameters};
+use prost::Message;
 
 use crate::{Particle, ParticleParameters};
 
 use super::{ParticleMap, ParticleMapAttribute};
 
-#[path = "particlemap_generated.rs"]
 #[allow(dead_code, unused_imports)]
 #[allow(clippy::all)]
-mod particlemap_generated;
+pub mod particlemap {
+    pub mod items {
+        include!(concat!(env!("OUT_DIR"), "/particlemap.items.rs"));
+    }
+}
 
 static RW_SPLITTER: &str = "|";
 
@@ -128,77 +128,50 @@ impl<T: ParticleMapAttributeRW> ParticleMap<T> {
     }
 
     pub fn read_from_bytes(bytes: Vec<u8>) -> Result<Self, Box<dyn Error>> {
-        let map = root_as_fbs_particle_map(&bytes)?;
-        let fbs_params = map.params().ok_or("Missing parameters")?;
+        let map = MsgParticleMap::decode(prost::bytes::Bytes::from(bytes))?;
+
         let params = ParticleParameters {
-            seed: fbs_params.seed(),
-            min_randomness: fbs_params.min_randomness(),
-            max_randomness: fbs_params.max_randomness(),
-            scale: fbs_params.scale(),
+            seed: map.params.unwrap().seed,
+            min_randomness: map.params.unwrap().min_randomness,
+            max_randomness: map.params.unwrap().max_randomness,
+            scale: map.params.unwrap().scale,
         };
 
-        let mut particles = HashMap::new();
-
-        if let Some(fbs_particles) = map.particles() {
-            for fbs_particle in fbs_particles.iter() {
-                let value = if let Some(value_str) = fbs_particle.value() {
-                    T::from_strs(&value_str.split(RW_SPLITTER).collect::<Vec<_>>())?
-                } else {
-                    return Err("Missing particle value".into());
-                };
-                particles.insert(
-                    Particle::new(fbs_particle.x(), fbs_particle.y(), params),
-                    value,
-                );
-            }
-        }
+        let particles = map
+            .particles
+            .iter()
+            .map(|particle| {
+                let value = T::from_strs(&particle.value.split(RW_SPLITTER).collect::<Vec<_>>())?;
+                Ok((Particle::new(particle.x, particle.y, params), value))
+            })
+            .collect::<Result<HashMap<Particle, T>, Box<dyn Error>>>()?;
 
         Ok(Self::new(params, particles))
     }
 
     pub fn write_to_bytes(&self) -> Result<Vec<u8>, Box<dyn Error>> {
-        let mut builder = FlatBufferBuilder::new();
-
-        // Create parameters
-        let fbs_params = FbsParticleParameters::create(
-            &mut builder,
-            &FbsParticleParametersArgs {
+        let map = MsgParticleMap {
+            params: Some(MsgParticleParameters {
                 seed: self.params.seed,
                 min_randomness: self.params.min_randomness,
                 max_randomness: self.params.max_randomness,
                 scale: self.params.scale,
-            },
-        );
-
-        // Create particles
-        let mut fbs_particles = Vec::new();
-        for (particle, value) in &self.particles {
-            let value_str = builder.create_string(&value.to_strings().join(RW_SPLITTER));
-            let fbs_particle = FbsParticle::create(
-                &mut builder,
-                &FbsParticleArgs {
+            }),
+            particles: self
+                .particles
+                .iter()
+                .map(|(particle, value)| MsgParticle {
                     x: particle.grid.0,
                     y: particle.grid.1,
-                    value: Some(value_str),
-                },
-            );
-            fbs_particles.push(fbs_particle);
-        }
+                    value: value.to_strings().join(RW_SPLITTER),
+                })
+                .collect(),
+        };
 
-        let fbs_particles_vec = builder.create_vector(&fbs_particles);
+        let mut buf = Vec::new();
+        map.encode(&mut buf)?;
 
-        // Create particle map
-        let fbs_map = FbsParticleMap::create(
-            &mut builder,
-            &FbsParticleMapArgs {
-                params: Some(fbs_params),
-                particles: Some(fbs_particles_vec),
-            },
-        );
-
-        finish_fbs_particle_map_buffer(&mut builder, fbs_map);
-
-        Ok(builder.finished_data().to_vec())
+        Ok(buf)
     }
 
     pub fn write_to_file(&self, file_path: &str) -> Result<(), Box<dyn Error>> {
